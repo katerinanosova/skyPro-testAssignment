f40c1019e9d505c028d2e4a05029829d330e5b49
katerinanosova <katerinanosova@gmail.com>
2024-01-24 15:55:57 +0000

    Added descending sort function

--- sky-test/src/API/userLoginApi.js
+++ sky-test/src/API/userLoginApi.js
@@ -8,9 +8,9 @@ export const getUsers = async (userLoginQuery) => {
     return data
 }
 
-export const getUsersSort = async (userLoginQuery) => {
+export const getUsersSort = async ({ query, sortOrder }) => {
 
-    const queryString = encodeURIComponent(`${userLoginQuery} in:login sort:repositories-asc`);
+    const queryString = encodeURIComponent(`${query} in:login sort:repositories-${sortOrder}`);
 
     const response = await fetch(`https://api.github.com/search/users?q=${queryString}`);
 
--- sky-test/src/Components/SearchResult/SearchResult.js
+++ sky-test/src/Components/SearchResult/SearchResult.js
@@ -10,8 +10,8 @@ export const SearchResult = ({ count, query, setFoundUsers }) => {
         setSortIsVisible(!sortIsVisible);
     }
 
-    const sortAscending = () => {
-        getUsersSort(query)
+    const sortResults = (sortOrder) => {
+        getUsersSort({ query, sortOrder })
         .then((users) => {setFoundUsers(users);})
         .catch((error) => {console.error('Ошибка при выполнении запроса:', error)})
     }
@@ -21,8 +21,8 @@ export const SearchResult = ({ count, query, setFoundUsers }) => {
             <S.SearchResultBox>Found {count} results for {query}</S.SearchResultBox>
             <S.SearchSortBox onClick={() => sortAppear()}>Sort by <span>repositories</span>
                 <S.SortList $sortIsVisible={sortIsVisible}>
-                    <S.SortListItem onClick={() => sortAscending()}>Ascending</S.SortListItem>
-                    <S.SortListItem>Descending</S.SortListItem>
+                    <S.SortListItem onClick={() => sortResults('asc')}>Ascending</S.SortListItem>
+                    <S.SortListItem onClick={() => sortResults('desc')}>Descending</S.SortListItem>
                 </S.SortList>
             </S.SearchSortBox>
         </S.SearchResultWrapper>
